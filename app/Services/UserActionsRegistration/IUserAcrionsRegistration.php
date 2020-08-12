a49ef9869543f827234f06039447aa756387f86d
aleksey <av.pavlow@gmail.com>
2020-08-12 20:02:05 +0300

    Сервис

---
+++ app/Services/UserActionsRegistration/IUserAcrionsRegistration.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\services\UserActionsRegistration;
+
+/**
+ * Интерфейс регистрации действий пользователя
+ * @package App\services\UserActionsRegistration
+ */
+interface IUserAcrionsRegistration
+{
+    /** Метод регистрации
+     * @return mixed
+     */
+    public function register();
+}
